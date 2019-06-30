0dfb7aba8940cf711d34f2fe47eb5e2acd01008b
jonik-do-it <d.arkhipov1997@yandex.ru>
2019-06-30 15:29:12 +0300

    adding recursive option

--- add.js
+++ add.js
@@ -26,3 +26,25 @@ async function addition(a, b) {
         return [...result].reverse().join('');
     })(a.split(''), b.split(''));
 }
+/* recursive option
+async function addition(a, b) {
+    if (typeof a !== 'string' || typeof b !== 'string') {
+        throw new TypeError(`Parameter ${a} or ${b} must be string's instance.`);
+    }
+    if (!a || !b) {
+        throw new Error(`Parameter ${a} or ${b} must be defined.`);
+    }
+    const re = /^\d+$/;
+    if (!re.test(a) || !re.test(b)) {
+        throw new Error(`Parameter ${a} or ${b} must be non-negative integers.`);
+    }
+    return (function getResult(a, b, result = '', added = 0) {
+        if (a.length !== 0 || b.length !== 0 || added !== 0) {
+            const sumOfTails = parseInt(a.pop() || '0') + parseInt(b.pop() || '0') + added;
+			const moreThanNine = sumOfTails > 9;
+			return getResult(a, b, (moreThanNine ? sumOfTails % 10 : sumOfTails) + result, moreThanNine ? 1 : 0);
+        }
+        return result;
+    })(a.split(''), b.split(''));
+}
+*/
